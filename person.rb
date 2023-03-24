73fdf50a4beddb26bef7047391173a7893e83dde
SabiMantock <sabimantock@gmail.com>
2023-03-24 13:07:28 +0000

    resolved requested changes

--- person.rb
+++ person.rb
@@ -21,8 +21,8 @@ class Person < Nameable
     @name
   end
 
-  def add_rental(person, date)
-    Rental.new(date, self, person)
+  def add_rental(book, date)
+    Rental.new(date, self, book)
   end
 
   private
